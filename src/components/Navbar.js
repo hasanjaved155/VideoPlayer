e3a15b2d1c0677de21689188bff4851d4a570092
JAVED HASAN <hasanjaved155@gmail.com>
2024-02-22 22:39:39 +0530

    neww

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -70,9 +70,9 @@ const Navbar = () => {
                         )
                         }
                     </div>
-                    <div className='mb-4' style={{ marginLeft: "400px", display: "flex" }}>
+                    <div className='mb-4' style={{ marginLeft: "460px", marginRight: "40px", display: "flex" }}>
                         <div>
-                            <button id="multiLevelDropdownButton" data-dropdown-toggle="multi-dropdown" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Dropdown button <svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+                            <button id="multiLevelDropdownButton" data-dropdown-toggle="multi-dropdown" style={{ marginTop: "2.2px" }} className="text-white bg-blue-700 h-12 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Category<svg className="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                                 <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="m1 1 4 4 4-4" />
                             </svg>
                             </button>
@@ -125,7 +125,6 @@ const Navbar = () => {
                                     value={searchQuery} onChange={handleSearch}
                                     required
                                 />
-                                <button type="submit" className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search</button>
                             </div>
                         </form>
 
